e8e22c0efa4e4cdbe7c47da21699a8916dabfb52
Mason Pike <mpike@flyamelia.com>
2024-02-02 17:57:02 +0100

    add generic 404 page

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,67 @@
-mod templates;
+use std::fs::File;
+use std::sync::Arc;
 
-use crate::templates::{HomeTemplate, ResumeTemplate};
 use askama::Template;
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use axum::{response::Html, routing::get, Router};
-use std::fs::File;
-use std::sync::Arc;
 use tower_http::services::ServeDir;
+use tracing_subscriber::fmt::layer;
 use tracing_subscriber::{filter, prelude::*};
+
+use crate::templates::{HomeTemplate, ResumeTemplate};
+
+mod templates;
 
 #[tokio::main]
 async fn main() {
-    let file = match File::create("debug.log") {
-        Ok(file) => file,
-        Err(error) => panic!("Error creating debug file: {:?}", error),
-    };
-    let debug_log = tracing_subscriber::fmt::layer().with_writer(Arc::new(file));
-
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::fmt::layer()
-                .pretty()
-                .with_filter(filter::LevelFilter::INFO)
-                .and_then(debug_log),
-        )
-        .init();
+    init_logger();
 
     let app = Router::new()
         .route("/", get(home))
         .route("/resume", get(resume))
-        .nest_service("/assets", ServeDir::new("assets"));
+        .nest_service("/assets", ServeDir::new("assets"))
+        .fallback(page_not_found);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
 }
 
+fn init_logger() {
+    let file = match File::create("debug.log") {
+        Ok(file) => file,
+        Err(error) => panic!("Error creating debug file: {:?}", error),
+    };
+
+    tracing_subscriber::registry()
+        .with(
+            layer()
+                .pretty()
+                .with_filter(filter::LevelFilter::INFO)
+                .and_then(layer().with_writer(Arc::new(file))),
+        )
+        .init();
+}
+
 async fn home() -> impl IntoResponse {
     tracing::debug!("Hello, Debug World!");
     tracing::warn!("Hello, Warning World!");
-    let template = HomeTemplate {};
-    (StatusCode::OK, Html(template.render().unwrap()).into_response())
+    (
+        StatusCode::OK,
+        Html((HomeTemplate {}).render().unwrap()).into_response(),
+    )
 }
 
 async fn resume() -> impl IntoResponse {
-    let template = ResumeTemplate {};
-    (StatusCode::OK, Html(template.render().unwrap()).into_response())
+    (
+        StatusCode::OK,
+        Html((ResumeTemplate {}).render().unwrap()).into_response(),
+    )
+}
+
+async fn page_not_found() -> impl IntoResponse {
+    (StatusCode::NOT_FOUND, "Page not found!")
 }
 
 #[cfg(test)]
